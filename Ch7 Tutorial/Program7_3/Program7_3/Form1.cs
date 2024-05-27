f16171afaca37efc55470b0f4cba4360be8e4597
shuochenghu <shuochenghu@gmail.com>
2024-05-27 15:00:38 +0800

    113-05-27

---
+++ Ch7 Tutorial/Program7_3/Program7_3/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Program7_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        const int SIZE = 18;
+        string[] acct= new string[SIZE];
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+
+    }
+}
